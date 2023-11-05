6bf27fd987bb2d22ac4e9b03d539591be9edde03
Ivan Golovachev <ivangol739@gmail.com>
2023-11-05 22:35:19 +0300

    first commit

---
+++ src/components/ShopItemClass.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import PropTypes from 'prop-types';
+import ItemModel from '../models/ItemModel';
+
+const propTypes = {
+  item: PropTypes.instanceOf(ItemModel).isRequired,
+};
+
+
+class ShopItemClass extends React.Component {
+  render() {
+    const {item} = this.props 
+      return (
+        <div class="main-content">
+          <h2>{item.brand}</h2>
+          <h1>{item.title}</h1>
+          <h3>{item.description}</h3>
+          <div class="description">
+            {item.descriptionFull}
+          </div>
+            <div class="highlight-window mobile">
+                <div class="highlight-overlay"></div>
+            </div>
+          <div class="divider"></div>
+            <div class="purchase-info">
+                <div class="price">{`${item.currency}${item.price.toFixed(2)}`}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div> 
+    )
+  }
+}
+
+ShopItemClass.propTypes = propTypes;
+
+export default ShopItemClass;
